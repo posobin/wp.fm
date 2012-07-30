bcf340f0b7eff78b940f1f4b6bc590d5ca100201
passick <posobin@gmail.com>
2012-07-30 22:51:18 +0400

    Icons. etc.

--- lastfm/App.xaml.cs
+++ lastfm/App.xaml.cs
@@ -54,7 +54,7 @@ namespace lastfm
                 // объекта PhoneApplicationService приложения значение Disabled.
                 // Внимание! Используйте только в режиме отладки. Приложение, в котором отключено обнаружение бездействия пользователя, будет продолжать работать
                 // и потреблять энергию батареи, когда телефон не будет использоваться.
-                PhoneApplicationService.Current.UserIdleDetectionMode = IdleDetectionMode.Disabled;
+                PhoneApplicationService.Current.UserIdleDetectionMode = IdleDetectionMode.Enabled;
             }
 
             Session.Scrobbles.CollectionChanged += new System.Collections.Specialized.NotifyCollectionChangedEventHandler(Scrobbling.Scrobbles_CollectionChanged);
