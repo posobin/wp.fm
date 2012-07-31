21563518d3ca650d69ca3860e42cb02d4fd34f44
passick <posobin@gmail.com>
2012-07-31 23:35:44 +0400

    Returned old keys. For binary releases now using another keys.

--- lastfm/secret_data.cs
+++ lastfm/secret_data.cs
@@ -17,7 +17,7 @@ namespace lastfm
 {
     private class secret_data
     {
-        protected const string api_key = "";
-        protected const string secret = "";
+        protected const string api_key = "85fc9119f3928f1a2c2c547b0247eb6d";
+        protected const string secret = "b0158e565140df0529a9a1f4ce15d9b5";
     }
 }
